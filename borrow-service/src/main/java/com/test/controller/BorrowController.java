84bce3838db64f24085c1756e19038a2bcddb9a4
繁星 <765472804@qq.com>
2023-03-01 17:08:07 +0800

    服务熔断 网关服务

--- borrow-service/src/main/java/com/test/controller/BorrowController.java
+++ borrow-service/src/main/java/com/test/controller/BorrowController.java
@@ -1,5 +1,6 @@
 package com.test.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.test.entity.UserBorrowDetail;
 import com.test.service.BorrowService;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.Collections;
 
 @RestController
 public class BorrowController {
@@ -14,8 +16,17 @@ public class BorrowController {
     @Resource
     BorrowService service;
 
+    //@HystrixCommand(fallbackMethod = "onError")    //使用@HystrixCommand来指定备选方案
     @RequestMapping("/borrow/{uid}")
     UserBorrowDetail findUserBorrows(@PathVariable("uid") int uid){
         return service.getUserBorrowDetailByUid(uid);
+    }
+
+    //备选方案，这里直接返回空列表了
+    //注意参数和返回值要和上面的一致
+    UserBorrowDetail onError(int uid){
+        System.out.println("执行补救措施");
+        //如果第一次进来就是降级补救,多次进入者可以理解熔断降级,不在进行正常方法的执行
+        return new UserBorrowDetail(null, Collections.emptyList());
     }
 }
---
+++ borrow-service/src/main/java/com/test/service/client/UserClient.java
@@ -0,0 +1,15 @@
+package com.test.service.client;
+
+import com.hudingwen.entity.User;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+//声明为userservice服务的HTTP请求客户端
+//fallback参数指定为我们刚刚编写的实现类
+@FeignClient(value = "userservice", fallback = UserFallbackClient.class)
+public interface UserClient {
+    //路径保证和其他微服务提供的一致即可
+    @RequestMapping("/user/{uid}")
+    User getUserById(@PathVariable("uid") int uid);  //参数和返回值也保持一致
+}
